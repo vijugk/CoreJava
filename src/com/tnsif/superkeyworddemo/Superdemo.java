9a7572d1162904dbf35e0980b367fcb43a55512b
vl483 <vl483@LAPTOP-SSME9S9D>
2025-11-03 13:02:08 +0530

    First commit

---
+++ src/com/tnsif/superkeyworddemo/Superdemo.java
@@ -0,0 +1,17 @@
+package com.tnsif.superkeyworddemo;
+// demo for super keyword
+//variables
+//method
+//constructor
+public class Superdemo {
+	
+	int a=60;// variable
+	
+	void drinking() {//method
+		System.out.println("tea");
+	}
+	 Superdemo(){ // constructor 
+		 System.out.println("welcome to java");
+	 }
+
+}
