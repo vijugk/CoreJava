d2151cadbc6761ff8df21f45c7e52cdd244c84a9
vl483 <vl483@LAPTOP-SSME9S9D>
2025-11-03 20:44:21 +0530

    First commit

---
+++ src/com/tnsif/polymorphismdemo/PNB.java
@@ -0,0 +1,16 @@
+package com.tnsif.polymorphismdemo;
+
+public class PNB extends RBI {
+
+	@Override
+	float rateofinterest() {
+		return 4.9f;
+	}
+	
+	public static void main(String[] args) {
+		PNB p=new PNB();
+		System.out.println(p.rateofinterest());
+	}
+	
+}
+
---
+++ src/com/tnsif/polymorphismdemo/RBI.java
@@ -0,0 +1,11 @@
+
+package com.tnsif.polymorphismdemo;
+
+public class RBI {
+
+	float rateofinterest() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
