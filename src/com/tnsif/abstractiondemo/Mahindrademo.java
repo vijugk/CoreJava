c57809b26932230e7adc3a10607db8c74c6b35ca
vl483 <vl483@LAPTOP-SSME9S9D>
2025-11-04 18:57:34 +0530

    First commit

---
+++ src/com/tnsif/abstractiondemo/Mahindrademo.java
@@ -0,0 +1,13 @@
+package com.tnsif.abstractiondemo;
+// demo for abstraction
+public abstract class Mahindrademo {
+	
+public void calling() {  // Concrete method
+	System.out.println("calling");
+}
+public abstract void AIIntegration(); // abstract method
+public abstract void Satellitecomm();
+public abstract void HealthMonitoring();
+	
+
+}
