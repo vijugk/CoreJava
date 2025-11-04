4e2f7accb725cdc77ff651cf436d8f1c0e9f37fd
vl483 <vl483@LAPTOP-SSME9S9D>
2025-11-04 11:00:47 +0530

    First commit

---
+++ src/com/tnsif/staticprogram/Staticblockdemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.staticprogram;
+// demo for static block
+
+public class Staticblockdemo {
+	static {
+		System.out.println("welcome to vkit");
+	}
+	public static void main(String[] args) {
+		System.out.println("welcome to java");
+	}
+
+}
